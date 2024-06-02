3c53fa1c7818bb8a06cd196ce7c991538baf543a
Benjamin Katz <benjamink@tingz.co.il>
2024-06-02 09:56:07 +0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // output: "export",
-  // basePath: "/nextjs-kaltura-test",
-  // images: {
-  //   unoptimized: true,
-  // },
+  // reactStrictMode: true,
+  output: "export",
+  basePath: "/nextjs-kaltura-test",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
